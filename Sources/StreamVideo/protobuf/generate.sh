b8b87f1f9f8bde294ffddbf4347a30d8a5a6ad07
martinmitrevski <martinmitrevski.oh@gmail.com>
2022-07-11 14:53:53 +0200

    Moved backend dependency to root

--- Sources/StreamVideo/protobuf/generate.sh
+++ Sources/StreamVideo/protobuf/generate.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
-cp -r `find backend/protobuf/** -type d` .
+cp -r `find ../../../backend/protobuf/** -type d` .
 protoc --swift_opt=FileNaming=DropPath --swift_out=. **/*.proto
 protoc --swift_out=:. --swiftwirp_out=:. --plugin="twirp/protoc-gen-swiftwirp"  **/*service.proto
 mv **/*twirp.swift .
-for folder in `find backend/protobuf/** -type d`
+for folder in `find ../../../backend/protobuf/** -type d`
 do
 	rm -rf `basename $folder`
 done
