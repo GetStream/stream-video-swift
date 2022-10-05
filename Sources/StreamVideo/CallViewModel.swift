5b0799529089c640146c791daf1440efa7377a29
martinmitrevski <martinmitrevski.oh@gmail.com>
2022-10-05 12:19:09 +0200

    Added docs for the CallViewModel

--- Sources/StreamVideo/CallViewModel.swift
+++ Sources/StreamVideo/CallViewModel.swift
@@ -71,12 +71,6 @@ open class CallViewModel: ObservableObject {
         subscribeToIncomingCalls()
     }
     
-    @objc private func checkForOngoingCall() {
-        if call == nil && callController?.call != nil {
-            call = callController?.call
-        }
-    }
-    
     public func setCallController(_ callController: CallController) {
         self.callController = callController
         call = callController.call
@@ -99,6 +93,7 @@ open class CallViewModel: ObservableObject {
         }
     }
     
+    /// Toggles the state of the microphone (muted vs unmuted).
     public func toggleMicrophoneEnabled() {
         // TODO: throw error
         guard let callController = callController else {
@@ -115,6 +110,7 @@ open class CallViewModel: ObservableObject {
         }
     }
     
+    /// Toggles the state of the camera (visible vs non-visible).
     public func toggleCameraPosition() {
         // TODO: throw error
         guard let callController = callController else {
@@ -125,6 +121,11 @@ open class CallViewModel: ObservableObject {
         callSettings = callSettings.withUpdatedCameraPosition(next)
     }
 
+    /// Starts a call with the provided info.
+    /// - Parameters:
+    ///  - callId: the id of the call.
+    ///  - type: optional type of a call. If not provided, the default would be used.
+    ///  - participants: list of participants that are part of the call.
     public func startCall(callId: String, type: String? = nil, participants: [UserInfo]) {
         outgoingCallMembers = participants
         callController = streamVideo.makeCallController(callType: callType(from: type), callId: callId)
@@ -132,6 +133,10 @@ open class CallViewModel: ObservableObject {
         enterCall(callId: callId, participantIds: participants.map(\.id))
     }
     
+    /// Joins an existing call with the provided info.
+    /// - Parameters:
+    ///  - callId: the id of the call.
+    ///  - type: optional type of a call. If not provided, the default would be used.
     public func joinCall(callId: String, type: String? = nil) {
         callController = streamVideo.makeCallController(callType: callType(from: type), callId: callId)
         enterCall(callId: callId, participantIds: participants.map(\.id))
@@ -148,15 +153,35 @@ open class CallViewModel: ObservableObject {
         }
     }
     
+    /// Renders the local video in the provided renderer.
+    /// - Parameter renderer: Any view (both UIKit and SwiftUI) implementing the `RTCVideoRenderer` protocol.
     public func renderLocalVideo(renderer: RTCVideoRenderer) {
         callController?.renderLocalVideo(renderer: renderer)
     }
     
+    /// Changes the track visibility for a participant (not visible if they go off-screen).
+    /// - Parameters:
+    ///  - participant: the participant whose track visibility would be changed.
+    ///  - isVisible: whether the track should be visible.
     public func changeTrackVisbility(for participant: CallParticipant, isVisible: Bool) {
         Task {
             await callController?.changeTrackVisibility(for: participant, isVisible: isVisible)
         }
     }
+    
+    /// Leaves the current call.
+    public func leaveCall() {
+        participantUpdates?.cancel()
+        participantUpdates = nil
+        call = nil
+        callParticipants = [:]
+        outgoingCallMembers = []
+        streamVideo.leaveCall()
+        currentEventsTask?.cancel()
+        callingState = .idle
+    }
+    
+    // MARK: - private
     
     private func enterCall(callId: String, participantIds: [String]) {
         guard let callController = callController else {
@@ -194,17 +219,6 @@ open class CallViewModel: ObservableObject {
         }
     }
     
-    public func leaveCall() {
-        participantUpdates?.cancel()
-        participantUpdates = nil
-        call = nil
-        callParticipants = [:]
-        outgoingCallMembers = []
-        streamVideo.leaveCall()
-        currentEventsTask?.cancel()
-        callingState = .idle
-    }
-    
     private func subscribeToIncomingCalls() {
         Task {
             for await incomingCall in streamVideo.incomingCalls() {
@@ -235,6 +249,12 @@ open class CallViewModel: ObservableObject {
         }
     }
     
+    @objc private func checkForOngoingCall() {
+        if call == nil && callController?.call != nil {
+            call = callController?.call
+        }
+    }
+    
     private func listenForParticipantEvents() {
         guard let call = call else {
             return
@@ -250,18 +270,16 @@ open class CallViewModel: ObservableObject {
     }
 }
 
-public struct User: Identifiable, Equatable {
-    public let name: String
-    public let token: String
-    public var id: String {
-        name
-    }
-}
-
+/// The state of the call.
 public enum CallingState: Equatable {
+    /// Call is not started (idle state).
     case idle
+    /// There's an incoming call.
     case incoming(IncomingCall)
+    /// There's an outgoing call.
     case outgoing
+    /// The user is in a call.
     case inCall
+    /// The user is trying to reconnect to a call.
     case reconnecting
 }
