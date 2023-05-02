a85a03df7bd3922aebde506f84b87c0a10b93665
Stefan <stefan.blos@gmail.com>
2023-05-02 17:21:30 +0200

    Fix wrong naming of audio rooms in CallType

--- Sources/StreamVideo/Models/CallType.swift
+++ Sources/StreamVideo/Models/CallType.swift
@@ -5,6 +5,6 @@
 extension String {
     public static let `default`: Self = "default"
     public static let development: Self = "development"
-    public static let audioRoom: Self = "audioRoom"
+    public static let audioRoom: Self = "audio_room"
     public static let livestream: Self = "livestream"
 }
