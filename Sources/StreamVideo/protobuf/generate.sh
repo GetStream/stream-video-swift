ebca18a1699a273c6e1795ba9072eb5514717b8f
martinmitrevski <martinmitrevski.oh@gmail.com>
2023-05-11 15:57:08 +0200

    Cleanup of unused files

--- Sources/StreamVideo/protobuf/generate.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-cp -r `find ../../../sfu/protobuf/** -type d` .
-protoc --swift_opt=FileNaming=PathToUnderscores --swift_out=. **/*.proto
-protoc --swift_out=:. --swiftwirp_out=:. --plugin="twirp/protoc-gen-swiftwirp"  **/*signal.proto
-mv **/*twirp.swift .
-for folder in `find ../../../sfu/protobuf/** -type d`
-do
-	rm -rf `basename $folder`
-done
