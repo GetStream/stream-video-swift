b2a9f888f5aad0db957d8596a9010e6278591811
Alexey Alter-Pesotskiy <a.alterpesotskiy@mail.ru>
2023-07-13 10:32:12 +0100

    Bump sdk version in source code during release (#113)

---
+++ Sources/StreamVideo/Generated/SystemEnvironment+Version.swift
@@ -0,0 +1,11 @@
+//
+// Copyright © 2023 Stream.io Inc. All rights reserved.
+//
+// ⚠️  Generated file, please use `fastlane release major|minor|patch` lanes
+
+import Foundation
+
+extension SystemEnvironment {
+  /// A Stream Video version.
+  public static let version: String = "0.1.0"
+}
