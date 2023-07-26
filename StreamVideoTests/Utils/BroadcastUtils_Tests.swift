c1b065f41ddeaa6cb302a83a81a265a8bb2ad91b
martinmitrevski <martinmitrevski.oh@gmail.com>
2023-07-26 12:02:46 +0200

    Added some tests

---
+++ StreamVideoTests/Utils/BroadcastUtils_Tests.swift
@@ -0,0 +1,65 @@
+//
+// Copyright © 2023 Stream.io Inc. All rights reserved.
+//
+
+@testable import StreamVideo
+import XCTest
+
+final class BroadcastUtils_Tests: XCTestCase {
+
+    func test_broadcastUtils_adjustBiggerWidth() {
+        // Given
+        let width: Int32 = 233
+        let height: Int32 = 177
+        let size: Int32 = 200
+        
+        // When
+        let result = BroadcastUtils.adjust(width: width, height: height, size: size)
+        
+        // Then
+        XCTAssert(result.width == size)
+        XCTAssert(result.height == 152) // Same ratio is kept
+    }
+    
+    func test_broadcastUtils_adjustBiggerHeight() {
+        // Given
+        let width: Int32 = 177
+        let height: Int32 = 233
+        let size: Int32 = 200
+        
+        // When
+        let result = BroadcastUtils.adjust(width: width, height: height, size: size)
+        
+        // Then
+        XCTAssert(result.height == size)
+        XCTAssert(result.width == 152) // Same ratio is kept
+    }
+    
+    func test_toSafeDimensions_notEven() {
+        // Given
+        let width: Int32 = 177
+        let height: Int32 = 233
+
+        // When
+        let result = BroadcastUtils.toSafeDimensions(width: width, height: height)
+        
+        // Then
+        XCTAssert(result.width == 178)
+        XCTAssert(result.height == 234)
+    }
+    
+    func test_broadcastUtils_aspectFit() {
+        // Given
+        let width: Int32 = 233
+        let height: Int32 = 177
+        let size: Int32 = 200
+        
+        // When
+        let result = BroadcastUtils.aspectFit(width: width, height: height, size: size)
+        
+        // Then
+        XCTAssert(result.width == size)
+        XCTAssert(result.height == 151)
+    }
+
+}
---
+++ StreamVideoTests/WebRTC/Screensharing/BroadcastObserver_Tests.swift
@@ -0,0 +1,47 @@
+//
+// Copyright © 2023 Stream.io Inc. All rights reserved.
+//
+
+@testable import StreamVideo
+import XCTest
+
+final class BroadcastObserver_Tests: StreamVideoTestCase {
+    
+    let notificationCenter: CFNotificationCenter = CFNotificationCenterGetDarwinNotifyCenter()
+
+    func test_broadcastObserver_states() async throws {
+        // Given
+        let broadcastObserver = BroadcastObserver()
+        broadcastObserver.observe()
+        
+        // Then
+        XCTAssert(broadcastObserver.broadcastState == .notStarted)
+        
+        // When
+        postNotification(BroadcastConstants.broadcastStartedNotification)
+        try await waitForCallEvent()
+        
+        // Then
+        XCTAssert(broadcastObserver.broadcastState == .started)
+        
+        // When
+        postNotification(BroadcastConstants.broadcastStoppedNotification)
+        try await waitForCallEvent()
+        
+        // Then
+        XCTAssert(broadcastObserver.broadcastState == .finished)
+    }
+    
+    //MARK: - private
+    
+    private func postNotification(_ name: String) {
+        CFNotificationCenterPostNotification(
+            notificationCenter,
+            CFNotificationName(rawValue: name as CFString),
+            nil,
+            nil,
+            true
+        )
+    }
+
+}
