7b2625dabb02e63e97846bbf2c7cc3d669631327
Ilias Pavlidakis <ilias.pavlidakis@getstream.io>
2023-09-04 13:25:05 +0300

    [Logs]Implement log viewer

---
+++ DemoApp/Sources/Components/MemoryLogDestination/LogQueue.swift
@@ -0,0 +1,43 @@
+//
+// Copyright © 2023 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+import StreamVideo
+
+enum LogQueue {
+    static let queue: Queue<LogDetails> = .init(maxCount: 1000)
+
+    static func insert(_ element: LogDetails) { queue.insert(element) }
+
+    static var maxCount: Int {
+        get { queue.maxCount }
+        set { queue.maxCount = newValue }
+    }
+}
+
+final class Queue<T>: ObservableObject {
+
+    @Published private(set) var elements: [T] = []
+    var maxCount: Int { didSet { resizeIfNeeded() } }
+    private let queue: DispatchQueue
+
+    init(maxCount: Int) {
+        self.maxCount = maxCount
+        self.queue = .init(label: "io.getstream.queue")
+    }
+
+    func insert(_ element: T) {
+        elements.insert(element, at: 0)
+        resizeIfNeeded()
+    }
+
+    private func resizeIfNeeded() {
+        queue.sync {
+            guard elements.endIndex > maxCount else {
+                return
+            }
+            elements = Array(elements[0...maxCount])
+        }
+    }
+}
---
+++ DemoApp/Sources/Components/MemoryLogDestination/MemoryLogDestination.swift
@@ -0,0 +1,17 @@
+//
+// Copyright © 2023 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+import StreamVideo
+
+final class MemoryLogDestination: BaseLogDestination {
+
+    override func process(logDetails: LogDetails) {
+        LogQueue.insert(logDetails)
+    }
+
+    override func write(message: String) {
+        /* No-Op */
+    }
+}
