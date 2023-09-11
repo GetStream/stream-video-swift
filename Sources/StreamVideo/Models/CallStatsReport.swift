b2d647010a62dba4dbb4550b47464337b8c1b294
martinmitrevski <martinmitrevski.oh@gmail.com>
2023-09-11 16:06:03 +0200

    Working implementation

--- Sources/StreamVideo/Models/CallStatsReport.swift
+++ Sources/StreamVideo/Models/CallStatsReport.swift
@@ -11,18 +11,38 @@ public struct CallStatsReport: Sendable {
     public let publisherRawStats: RTCStatisticsReport?
     public let subscriberStats: AggregatedStatsReport
     public let subscriberRawStats: RTCStatisticsReport?
-    //TODO: check for participants stats report
-    public let timestamp: Int
+    public let participantsStats: ParticipantsStats
+    public let timestamp: Double
+}
+
+public struct ParticipantsStats: Sendable {
+    public let report: [String: BaseStats]
+}
+
+public struct BaseStats: Sendable {
+    public let bytesSent: Int
+    public let bytesReceived: Int
+    public let codec: String
+    public let currentRoundTripTime: Double
+    public let frameWidth: Int
+    public let frameHeight: Int
+    public let framesPerSecond: Int
+    public let jitter: Double
+    public let kind: String
+    public let qualityLimitationReason: String
+    public let rid: String
+    public let ssrc: Int
+    public let isPublisher: Bool
 }
 
 public struct AggregatedStatsReport: Sendable {
     public let totalBytesSent: Int
     public let totalBytesReceived: Int
-    public let averageJitterInMs: Int
-    public let averageRoundTripTimeInMs: Int
+    public let averageJitterInMs: Double
+    public let averageRoundTripTimeInMs: Double
     public let qualityLimitationReasons: String
     public let highestFrameWidth: Int
     public let highestFrameHeight: Int
     public let highestFramesPerSecond: Int
-    public let timestamp: Int
+    public let timestamp: Double
 }
--- Sources/StreamVideo/WebRTC/StatsReporter.swift
+++ Sources/StreamVideo/WebRTC/StatsReporter.swift
@@ -11,48 +11,139 @@ enum StatsReporter {
         from reports: [RTCStatisticsReport?],
         datacenter: String
     ) -> CallStatsReport {
-        CallStatsReport(
+        let participantsStats = participantsStats(from: reports)
+        let timestamp = reports.first??.timestamp_us ?? 0
+        let publisherReport: RTCStatisticsReport? = reports[safe: 0] ?? nil
+        let subscriberReport: RTCStatisticsReport? = reports[safe: 1] ?? nil
+        return CallStatsReport(
             datacenter: datacenter,
-            publisherStats: publisherStats(from: reports),
-            publisherRawStats: reports[0], //TODO: safer
-            subscriberStats: subscriberStats(from: reports),
-            subscriberRawStats: reports[1], //TODO: safer
-            timestamp: 0 //TODO: this
+            publisherStats: publisherStats(from: participantsStats, timestamp: timestamp),
+            publisherRawStats: publisherReport,
+            subscriberStats: subscriberStats(from: participantsStats, timestamp: timestamp),
+            subscriberRawStats: subscriberReport,
+            participantsStats: participantsStats,
+            timestamp: timestamp
         )
+    }
+    
+    private static func participantsStats(
+        from reports: [RTCStatisticsReport?]
+    ) -> ParticipantsStats {
+        guard reports.count >= 2 else {
+            return ParticipantsStats(report: [:])
+        }
+        var participants = [String: BaseStats]()
+        for (index, report) in reports.enumerated() {
+            let values = report?.statistics
+            var current = [String: [String: Any]]()
+            if let values {
+                for (_, value) in values {
+                    let stats = value.values
+                    if let trackIdentifier = stats[StatsConstants.trackIdentifier] as? String,
+                        stats[StatsConstants.kind] as? String == "video" {
+                        if var existing = current[trackIdentifier] {
+                            for (key, value) in stats {
+                                existing[key] = value
+                            }
+                            current[trackIdentifier] = existing
+                        } else {
+                            current[trackIdentifier] = stats
+                        }
+                    }
+                }
+            }
+            for (key, stats) in current {
+                let baseStats = BaseStats(
+                    bytesSent: stats[StatsConstants.bytesSent] as? Int ?? 0,
+                    bytesReceived: stats[StatsConstants.bytesReceived] as? Int ?? 0,
+                    codec: stats[StatsConstants.codecId] as? String ?? "",
+                    currentRoundTripTime: stats[StatsConstants.currentRoundTripTime] as? Double ?? 0,
+                    frameWidth: stats[StatsConstants.frameWidth] as? Int ?? 0,
+                    frameHeight: stats[StatsConstants.frameHeight] as? Int ?? 0,
+                    framesPerSecond: stats[StatsConstants.framesPerSecond] as? Int ?? 0,
+                    jitter: stats[StatsConstants.jitter] as? Double ?? 0,
+                    kind: stats[StatsConstants.kind] as? String ?? "video",
+                    qualityLimitationReason: stats[StatsConstants.qualityLimitationReason] as? String ?? "",
+                    rid: stats[StatsConstants.rid] as? String ?? "",
+                    ssrc: stats[StatsConstants.ssrc] as? Int ?? 0,
+                    isPublisher: index == 0
+                )
+                participants[key] = baseStats
+            }
+        }
+        
+        return ParticipantsStats(report: participants)
     }
     
     private static func publisherStats(
-        from reports: [RTCStatisticsReport?]
+        from stats: ParticipantsStats,
+        timestamp: Double
     ) -> AggregatedStatsReport {
-        //TODO: implement
-        AggregatedStatsReport(
-            totalBytesSent: 0,
-            totalBytesReceived: 0,
-            averageJitterInMs: 0,
-            averageRoundTripTimeInMs: 0,
-            qualityLimitationReasons: "",
-            highestFrameWidth: 0,
-            highestFrameHeight: 0,
-            highestFramesPerSecond: 0,
-            timestamp: 0
-        )
+        let filteredStats = stats.report.values.filter { $0.isPublisher }
+        return aggregatedReport(from: filteredStats, timestamp: timestamp)
     }
     
     private static func subscriberStats(
-        from reports: [RTCStatisticsReport?]
+        from stats: ParticipantsStats,
+        timestamp: Double
     ) -> AggregatedStatsReport {
-        //TODO: implement
-        AggregatedStatsReport(
-            totalBytesSent: 0,
-            totalBytesReceived: 0,
-            averageJitterInMs: 0,
-            averageRoundTripTimeInMs: 0,
-            qualityLimitationReasons: "",
-            highestFrameWidth: 0,
-            highestFrameHeight: 0,
-            highestFramesPerSecond: 0,
-            timestamp: 0
-        )
+        let filteredStats = stats.report.values.filter { $0.isPublisher == false }
+        return aggregatedReport(from: filteredStats, timestamp: timestamp)
     }
     
+    private static func aggregatedReport(
+        from stats: [BaseStats],
+        timestamp: Double
+    ) -> AggregatedStatsReport {
+        var totalBytesSent = 0
+        var totalBytesReceived = 0
+        var totalJitter: Double = 0
+        var totalRoundTripTime: Double = 0
+        var highestFrameWidth = 0
+        var highestFrameHeight = 0
+        var highestFps = 0
+        var qualityReasons = ""
+        for stat in stats {
+            totalBytesSent += stat.bytesSent
+            totalBytesReceived += stat.bytesReceived
+            totalJitter += stat.jitter
+            totalRoundTripTime += stat.currentRoundTripTime
+            highestFrameWidth = max(stat.frameWidth, highestFrameWidth)
+            highestFrameHeight = max(stat.frameHeight, highestFrameHeight)
+            highestFps = max(stat.framesPerSecond, highestFps)
+            if !stat.qualityLimitationReason.isEmpty {
+                qualityReasons += "\(stat.qualityLimitationReason)|"
+            }
+        }
+        let totalEntries = Double(stats.count)
+        let averageJitter = totalJitter / totalEntries
+        let averageRoundTripTime = totalRoundTripTime / totalEntries
+        return AggregatedStatsReport(
+            totalBytesSent: totalBytesSent,
+            totalBytesReceived: totalBytesReceived,
+            averageJitterInMs: averageJitter,
+            averageRoundTripTimeInMs: averageRoundTripTime,
+            qualityLimitationReasons: qualityReasons,
+            highestFrameWidth: highestFrameWidth,
+            highestFrameHeight: highestFrameHeight,
+            highestFramesPerSecond: highestFps,
+            timestamp: timestamp
+        )
+    }
+}
+
+enum StatsConstants {
+    static let trackIdentifier = "trackIdentifier"
+    static let bytesSent = "bytesSent"
+    static let bytesReceived = "bytesReceived"
+    static let codecId = "codecId"
+    static let currentRoundTripTime = "currentRoundTripTime"
+    static let frameWidth = "frameWidth"
+    static let frameHeight = "frameHeight"
+    static let framesPerSecond = "framesPerSecond"
+    static let jitter = "jitter"
+    static let kind = "kind"
+    static let qualityLimitationReason = "qualityLimitationReason"
+    static let rid = "rid"
+    static let ssrc = "ssrc"
 }
