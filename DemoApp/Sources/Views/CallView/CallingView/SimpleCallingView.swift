15f0e55cc4fc1e475e15b237f3087774fb0a8ee5
Ilias Pavlidakis <ilias.pavlidakis@getstream.io>
2023-12-21 15:41:25 +0200

    Fix alerts

--- DemoApp/Sources/Views/CallView/CallingView/SimpleCallingView.swift
+++ DemoApp/Sources/Views/CallView/CallingView/SimpleCallingView.swift
@@ -75,21 +75,21 @@ struct SimpleCallingView: View {
                     .disabled(appState.loading || text.isEmpty)
                 }
                 .disabled(appState.loading || text.isEmpty)
-            }
-            .alert(isPresented: $showChangeEnvironmentPrompt) {
-                if let url = changeEnvironmentPromptForURL {
-                    return Alert(
-                        title: Text("Change environment"),
-                        message: Text("In order to access the call you scanned, we will need to change the environment you are logged in. Would you like to proceed?"),
-                        primaryButton: .default(Text("OK")) { Router.shared.handle(url: url) },
-                        secondaryButton: .cancel()
-                    )
-                } else {
-                    return Alert(
-                        title: Text("Invalid URL"),
-                        message: Text("The URL contained in the QR you scanned was invalid. Please try again."),
-                        dismissButton: .cancel()
-                    )
+                .alert(isPresented: $showChangeEnvironmentPrompt) {
+                    if let url = changeEnvironmentPromptForURL {
+                        return Alert(
+                            title: Text("Change environment"),
+                            message: Text("In order to access the call you scanned, we will need to change the environment you are logged in. Would you like to proceed?"),
+                            primaryButton: .default(Text("OK")) { Router.shared.handle(url: url) },
+                            secondaryButton: .cancel()
+                        )
+                    } else {
+                        return Alert(
+                            title: Text("Invalid URL"),
+                            message: Text("The URL contained in the QR you scanned was invalid. Please try again."),
+                            dismissButton: .cancel()
+                        )
+                    }
                 }
             }
 
