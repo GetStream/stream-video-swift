2bb3c9460ebefede0e9a88a166392867b5bca657
Ilias Pavlidakis <ilias.pavlidakis@getstream.io>
2024-02-05 13:04:40 +0200

    Fix failing tests

--- StreamVideoSwiftUITests/CallView/ParticipantsGridLayout_Tests.swift
+++ StreamVideoSwiftUITests/CallView/ParticipantsGridLayout_Tests.swift
@@ -11,7 +11,10 @@ import XCTest
 @MainActor
 final class ParticipantsGridLayout_Tests: StreamVideoUITestCase {
     
-    nonisolated private lazy var callController = CallController_Mock(
+    private var mockedOrientation: StreamDeviceOrientation! = .portrait
+    private lazy var orientationAdapter: StreamDeviceOrientationAdapter! = .init { self.mockedOrientation }
+
+    private lazy var callController: CallController_Mock! = CallController_Mock(
         defaultAPI: DefaultAPI(
             basePath: "test.com",
             transport: httpClient as! HTTPClient_Mock,
@@ -24,24 +27,32 @@ final class ParticipantsGridLayout_Tests: StreamVideoUITestCase {
         videoConfig: VideoConfig(),
         cachedLocation: nil
     )
-    
-    override func setUpWithError() throws {
-        try super.setUpWithError()
-        
+
+    override func setUp() {
+        super.setUp()
         let streamVideo = StreamVideo.mock(httpClient: httpClient, callController: callController)
         streamVideoUI = StreamVideoUI(streamVideo: streamVideo)
+        InjectedValues[\.orientationAdapter] = orientationAdapter
     }
-    
+
+    override func tearDown() {
+        mockedOrientation = nil
+        orientationAdapter = nil
+        callController = nil
+        super.tearDown()
+    }
+
     private lazy var call = streamVideoUI?.streamVideo.call(callType: callType, callId: callId)
     
     func test_grid_participantWithAudio_snapshot() {
+        mockedOrientation = .portrait
+
         for count in gridParticipants {
             let layout = ParticipantsGridLayout(
                 viewFactory: DefaultViewFactory.shared,
                 call: call,
                 participants: ParticipantFactory.get(count, withAudio: true),
                 availableFrame: .init(origin: .zero, size: defaultScreenSize),
-                orientation: .portrait,
                 onChangeTrackVisibility: {_,_ in }
             )
             AssertSnapshot(layout, variants: snapshotVariants, suffix: "with_\(count)_participants")
@@ -49,13 +60,14 @@ final class ParticipantsGridLayout_Tests: StreamVideoUITestCase {
     }
     
     func test_grid_participantWithoutAudio_snapshot() {
+        mockedOrientation = .portrait
+
         for count in gridParticipants {
             let layout = ParticipantsGridLayout(
                 viewFactory: DefaultViewFactory.shared,
                 call: call,
                 participants: ParticipantFactory.get(count, withAudio: false),
                 availableFrame: .init(origin: .zero, size: defaultScreenSize),
-                orientation: .portrait,
                 onChangeTrackVisibility: {_,_ in }
             )
             AssertSnapshot(layout, variants: snapshotVariants, suffix: "with_\(count)_participants")
@@ -63,6 +75,8 @@ final class ParticipantsGridLayout_Tests: StreamVideoUITestCase {
     }
     
     func test_grid_participantsConnectionQuality_snapshot() throws {
+        mockedOrientation = .portrait
+
         for quality in connectionQuality {
             let count = gridParticipants.last!
             let layout = ParticipantsGridLayout(
@@ -70,7 +84,6 @@ final class ParticipantsGridLayout_Tests: StreamVideoUITestCase {
                 call: call,
                 participants: ParticipantFactory.get(count, connectionQuality: quality),
                 availableFrame: .init(origin: .zero, size: defaultScreenSize),
-                orientation: .portrait,
                 onChangeTrackVisibility: {_,_ in }
             )
             AssertSnapshot(layout, variants: snapshotVariants, suffix: "\(quality)")
@@ -78,6 +91,8 @@ final class ParticipantsGridLayout_Tests: StreamVideoUITestCase {
     }
     
     func test_grid_participantsSpeaking_snapshot() {
+        mockedOrientation = .portrait
+
         for count in gridParticipants {
             let participants = ParticipantFactory.get(count, speaking: true)
             var dict = [String: CallParticipant]()
@@ -90,7 +105,6 @@ final class ParticipantsGridLayout_Tests: StreamVideoUITestCase {
                 call: call,
                 participants: participants,
                 availableFrame: .init(origin: .zero, size: defaultScreenSize),
-                orientation: .portrait,
                 onChangeTrackVisibility: {_,_ in }
             )
             AssertSnapshot(layout, variants: snapshotVariants, suffix: "with_\(count)_participants")
