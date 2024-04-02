bd0814caa482c77fc11a461656d0e31f75b4d62b
Ilias Pavlidakis <ilias.pavlidakis@getstream.io>
2024-04-02 12:10:39 +0300

    [Enhancement]Remove Nuke dependency

--- Sources/StreamVideoSwiftUI/Utils/UserAvatar.swift
+++ Sources/StreamVideoSwiftUI/Utils/UserAvatar.swift
@@ -7,20 +7,26 @@ import SwiftUI
 @available(iOS 14.0, *)
 public struct UserAvatar<Failback: View>: View {
 
-    public typealias FailbackProvider = () -> Failback
+    public typealias FailbackProvider = () -> _ConditionalContent<Failback, EmptyView>
 
     public var imageURL: URL?
     public var size: CGFloat
-    public var failbackProvider: FailbackProvider?
+    public var failbackProvider: FailbackProvider
 
-    public init(imageURL: URL?, size: CGFloat, failbackProvider: FailbackProvider?) {
+    public init(imageURL: URL?, size: CGFloat, failbackProvider: (() -> Failback)?) {
         self.imageURL = imageURL
         self.size = size
-        self.failbackProvider = failbackProvider
+        self.failbackProvider = {
+            if let failbackProvider {
+                return ViewBuilder.buildEither(first: failbackProvider())
+            } else {
+                return ViewBuilder.buildEither(second: EmptyView())
+            }
+        }
     }
     
     public var body: some View {
-        StreamLazyImage(imageURL: imageURL, failback: failbackProvider)
+        StreamLazyImage(imageURL: imageURL, placeholder: failbackProvider)
             .frame(width: size, height: size)
             .clipShape(Circle())
     }
