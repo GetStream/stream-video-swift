19901859ee9ddae230f06061e13a7fb4b939ec53
Ilias Pavlidakis <ilias.pavlidakis@getstream.io>
2024-04-11 16:05:36 +0300

    Add comments

--- Sources/StreamVideoSwiftUI/Utils/ReusePool.swift
+++ Sources/StreamVideoSwiftUI/Utils/ReusePool.swift
@@ -5,13 +5,27 @@
 import Foundation
 import StreamVideo
 
+/// A generic pool for reusing instances of elements that conform to AnyObject and Hashable protocols.
+/// The pool will align stretch and/or tight its capacity depending on how its objects are being used.
+///
+/// - Important: Callers are responsible for informing the reusePool when an object is to be released.
 final class ReusePool<Element: AnyObject & Hashable> {
+    /// The initial capacity of the pool.
     private let initialCapacity: Int
+    /// The factory closure used to create new instances of elements.
     private let factory: () -> Element
+    /// Queue for thread-safe operations.
     private let queue = UnfairQueue()
+    /// Array to store available elements for reuse.
     private var available: [Element] = []
+    /// Set to track elements currently in use.
     private var inUse: Set<Element> = []
 
+    /// Initializes the pool with a specified initial capacity and a factory closure to create elements.
+    ///
+    /// - Parameters:
+    ///   - initialCapacity: The initial capacity of the pool (default is 10).
+    ///   - factory: A closure that returns a new instance of an element.
     init(initialCapacity: Int = 10, factory: @escaping () -> Element) {
         self.initialCapacity = initialCapacity
         self.factory = factory
@@ -23,6 +37,9 @@ final class ReusePool<Element: AnyObject & Hashable> {
         }
     }
 
+    /// Acquires an element from the pool for use.
+    ///
+    /// - Returns: An element from the pool.
     func acquire() -> Element {
         var element: Element!
 
@@ -41,6 +58,9 @@ final class ReusePool<Element: AnyObject & Hashable> {
         return element
     }
 
+    /// Releases an element back to the pool for reuse.
+    ///
+    /// - Parameter element: The element to release.
     func release(_ element: Element) {
         queue.sync {
             if inUse.contains(element), available.endIndex < initialCapacity {
@@ -54,6 +74,7 @@ final class ReusePool<Element: AnyObject & Hashable> {
         }
     }
 
+    /// Releases all elements currently in use back to the pool for reuse.
     func releaseAll() {
         queue.sync {
             for element in inUse {
