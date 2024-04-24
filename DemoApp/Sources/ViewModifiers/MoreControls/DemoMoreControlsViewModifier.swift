0cdb4d9963d154c9f1dd221617d5d8a5c2b6f851
Ilias Pavlidakis <ilias.pavlidakis@getstream.io>
2024-04-24 14:00:34 +0300

    Add docs

--- DemoApp/Sources/ViewModifiers/MoreControls/DemoMoreControlsViewModifier.swift
+++ DemoApp/Sources/ViewModifiers/MoreControls/DemoMoreControlsViewModifier.swift
@@ -18,13 +18,9 @@ private struct DemoMoreControlsViewModifier: ViewModifier {
     @Injected(\.localParticipantSnapshotViewModel) var localParticipantSnapshotViewModel
 
     @State private var isStatsPresented = false
-    @State private var isTranscriptionAvailable = false
-    @State private var isTranscribing = false
 
     init(viewModel: CallViewModel) {
         self.viewModel = viewModel
-        isTranscriptionAvailable = viewModel.call?.state.settings?.transcription.mode != .disabled
-        isTranscribing = viewModel.call?.state.transcribing ?? false
         localParticipantSnapshotViewModel.call = viewModel.call
     }
 
@@ -119,40 +115,7 @@ private struct DemoMoreControlsViewModifier: ViewModifier {
                                 )
                             }
 
-                            Group {
-                                if isTranscriptionAvailable {
-                                    DemoMoreControlListButtonView(
-                                        action: {
-                                            Task {
-                                                do {
-                                                    if isTranscribing {
-                                                        try await viewModel.call?.stopTranscription()
-                                                    } else {
-                                                        try await viewModel.call?.startTranscription()
-                                                    }
-                                                } catch {
-                                                    log.error(error)
-                                                }
-                                            }
-                                        },
-                                        label: isTranscribing ? "Disable Transcription" : "Transcription"
-                                    ) {
-                                        Image(
-                                            systemName: isTranscribing
-                                                ? "captions.bubble.fill"
-                                                : "captions.bubble"
-                                        )
-                                    }
-                                    .onReceive(viewModel.call?.state.$transcribing) { isTranscribing = $0 }
-                                }
-                            }
-                            .onReceive(viewModel.call?.state.$settings) {
-                                guard let mode = $0?.transcription.mode else {
-                                    isTranscriptionAvailable = false
-                                    return
-                                }
-                                isTranscriptionAvailable = mode != .disabled
-                            }
+                            DemoTranscriptionButtonView(viewModel: viewModel)
 
                             DemoMoreControlListButtonView(
                                 action: { isStatsPresented = true },
@@ -169,6 +132,56 @@ private struct DemoMoreControlsViewModifier: ViewModifier {
                     )
                 }
             }
+    }
+}
+
+struct DemoTranscriptionButtonView: View {
+
+    @ObservedObject var viewModel: CallViewModel
+    @State private var isTranscriptionAvailable = false
+    @State private var isTranscribing = false
+
+    init(viewModel: CallViewModel) {
+        self.viewModel = viewModel
+        isTranscriptionAvailable = (viewModel.call?.state.settings?.transcription.mode ?? .disabled) != .disabled
+        isTranscribing = viewModel.call?.state.transcribing == true
+    }
+
+    var body: some View {
+        Group {
+            if isTranscriptionAvailable {
+                DemoMoreControlListButtonView(
+                    action: {
+                        Task {
+                            do {
+                                if isTranscribing {
+                                    try await viewModel.call?.stopTranscription()
+                                } else {
+                                    try await viewModel.call?.startTranscription()
+                                }
+                            } catch {
+                                log.error(error)
+                            }
+                        }
+                    },
+                    label: isTranscribing ? "Disable Transcription" : "Transcription"
+                ) {
+                    Image(
+                        systemName: isTranscribing
+                            ? "captions.bubble.fill"
+                            : "captions.bubble"
+                    )
+                }
+                .onReceive(viewModel.call?.state.$transcribing) { isTranscribing = $0 }
+            }
+        }
+        .onReceive(viewModel.call?.state.$settings) {
+            guard let mode = $0?.transcription.mode else {
+                isTranscriptionAvailable = false
+                return
+            }
+            isTranscriptionAvailable = mode != .disabled
+        }
     }
 }
 
---
+++ DocumentationTests/DocumentationTests/DocumentationTests/05-ui-cookbook/18-transcriptions.swift
@@ -0,0 +1,63 @@
+import StreamVideo
+import StreamVideoSwiftUI
+import SwiftUI
+import Combine
+import AVFoundation
+
+@MainActor
+fileprivate func content() {
+    container {
+        struct TranscriptionButtonView: View {
+            
+            @ObservedObject var viewModel: CallViewModel
+            @State private var isTranscriptionAvailable = false
+            @State private var isTranscribing = false
+            
+            init(viewModel: CallViewModel) {
+                self.viewModel = viewModel
+                self.isTranscriptionAvailable = (viewModel.call?.state.settings?.transcription.mode ?? .disabled) != .disabled
+                self.isTranscribing = viewModel.call?.state.transcribing == true
+            }
+            
+            var body: some View {
+                if let call = viewModel.call {
+                    Group {
+                        if isTranscriptionAvailable {
+                            Button {
+                                Task {
+                                    do {
+                                        if isTranscribing {
+                                            try await viewModel.call?.stopTranscription()
+                                        } else {
+                                            try await viewModel.call?.startTranscription()
+                                        }
+                                    } catch {
+                                        log.error(error)
+                                    }
+                                }
+                            } label: {
+                                Label {
+                                    Text(isTranscribing ? "Disable Transcription" : "Transcription")
+                                } icon: {
+                                    Image(
+                                        systemName: isTranscribing
+                                        ? "captions.bubble.fill"
+                                        : "captions.bubble"
+                                    )
+                                }
+                            }
+                            .onReceive(call.state.$transcribing) { isTranscribing = $0 }
+                        }
+                    }
+                    .onReceive(call.state.$settings) {
+                        guard let mode = $0?.transcription.mode else {
+                            isTranscriptionAvailable = false
+                            return
+                        }
+                        isTranscriptionAvailable = mode != .disabled
+                    }
+                }
+            }
+        }
+    }
+}
