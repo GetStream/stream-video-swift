c2555268bd1cbe635e37406f2218f9c2571855a6
Ilias Pavlidakis <ilias.pavlidakis@getstream.io>
2024-05-21 12:36:51 +0300

    Implement StateMachine

---
+++ Sources/StreamVideo/Utils/StateMachine/Stage.swift
@@ -0,0 +1,19 @@
+//
+// Copyright © 2024 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+
+public protocol StateMachineStage: CustomStringConvertible {
+    associatedtype ID: Hashable
+    typealias Transition = (Self) -> Void
+    var id: ID { get }
+
+    var transition: Transition? { get set }
+
+    func transition(from previousStage: Self) -> Self?
+}
+
+extension StateMachineStage {
+    public var description: String { "\(type(of: self)):\(id)" }
+}
