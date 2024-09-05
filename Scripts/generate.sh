974a482831da65614535a22c30e7dedce4966d48
Ali Momen Sani <ali.momensani91@gmail.com>
2024-09-05 12:13:38 +0200

    better generate script

---
+++ Scripts/generate.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+SOURCE_PATH=../chat
+
+if [ ! -d $SOURCE_PATH ]
+then
+  echo "cannot find chat path on the parent folder (${SOURCE_PATH}), do you have a copy of the API source?";
+  exit 1;
+fi
+
+set -ex
+
+# remove old generated code
+rm -rf ./Sources/StreamVideo/OpenApi/generated/Models/*
+
+# cd in API repo, generate new spec and then generate code from it
+# make openapi ; 
+( cd $SOURCE_PATH ; go run ./cmd/chat-manager openapi generate-client --language swift --spec ./releases/video-openapi-clientside.yaml --output ../stream-video-swift/Sources/StreamVideo/OpenApi/generated/ )
+
+# format the generated code
+mint run swiftformat Sources/StreamVideo/OpenApi/generated
