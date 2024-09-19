4ed24f1a9b639108ea1ac9533efcae5c0d72ad36
Ilias Pavlidakis <ilias.pavlidakis@getstream.io>
2024-09-19 21:53:06 +0300

    [Fix]Track orientation issue (#534)

---
+++ StreamVideoTests/Mock/MockRTCVideoCapturerDelegate.swift
@@ -0,0 +1,22 @@
+//
+// Copyright © 2024 Stream.io Inc. All rights reserved.
+//
+
+//
+//  MockRTCVideoCapturerDelegate.swift
+//  StreamVideo
+//
+//  Created by Ilias Pavlidakis on 19/9/24.
+//
+import StreamWebRTC
+
+final class MockRTCVideoCapturerDelegate: NSObject, RTCVideoCapturerDelegate {
+    private(set) var didCaptureWasCalledWith: (capturer: RTCVideoCapturer, frame: RTCVideoFrame)?
+
+    func capturer(
+        _ capturer: RTCVideoCapturer,
+        didCapture frame: RTCVideoFrame
+    ) {
+        didCaptureWasCalledWith = (capturer, frame)
+    }
+}
---
+++ StreamVideoTests/Utilities/Extensions/CVPixelBuffer+Convenience.swift
@@ -0,0 +1,46 @@
+//
+// Copyright © 2024 Stream.io Inc. All rights reserved.
+//
+
+import CoreVideo
+import Foundation
+import StreamVideo
+
+extension CVPixelBuffer {
+    static func make(bufferSize: CGSize = .init(width: 100, height: 100)) throws -> CVPixelBuffer {
+        var cvPool: CVPixelBufferPool?
+        let poolAttributes: [String: Any] = [
+            kCVPixelBufferPoolMinimumBufferCountKey as String: 5
+        ]
+        let pixelBufferAttributes: [String: Any] = [
+            kCVPixelBufferPixelFormatTypeKey as String: Int(kCVPixelFormatType_32BGRA),
+            kCVPixelBufferWidthKey as String: Int(bufferSize.width),
+            kCVPixelBufferHeightKey as String: Int(bufferSize.height),
+            kCVPixelBufferIOSurfacePropertiesKey as String: [:]
+        ]
+        CVPixelBufferPoolCreate(
+            nil,
+            poolAttributes as CFDictionary,
+            pixelBufferAttributes as CFDictionary,
+            &cvPool
+        )
+        guard let pool = cvPool else {
+            throw ClientError()
+        }
+
+        var pixelBuffer: CVPixelBuffer?
+        let error = CVPixelBufferPoolCreatePixelBuffer(
+            nil,
+            pool,
+            &pixelBuffer
+        )
+
+        if error == kCVReturnWouldExceedAllocationThreshold {
+            throw ClientError("\(kCVReturnWouldExceedAllocationThreshold)")
+        } else if let pixelBuffer {
+            return pixelBuffer
+        } else {
+            throw ClientError()
+        }
+    }
+}
