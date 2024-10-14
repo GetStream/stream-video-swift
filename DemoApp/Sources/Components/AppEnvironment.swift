34af1e260290777439f32d95ae5e6fca0f58716d
Ilias Pavlidakis <ilias.pavlidakis@getstream.io>
2024-10-14 17:30:09 +0300

    [Demo]Thermal State improved management

--- DemoApp/Sources/Components/AppEnvironment.swift
+++ DemoApp/Sources/Components/AppEnvironment.swift
@@ -442,3 +442,23 @@ extension AppEnvironment {
 
     static var autoLeavePolicy: AutoLeavePolicy = .default
 }
+
+extension AppEnvironment {
+
+    enum ThermalStateManagement: Hashable, Debuggable {
+        case enabled, disabled
+
+        var title: String {
+            switch self {
+            case .enabled:
+                return "Enabled"
+            case .disabled:
+                return "Disabled"
+            }
+        }
+    }
+
+    static var thermalStateManagement: ThermalStateManagement = {
+        .enabled
+    }()
+}
