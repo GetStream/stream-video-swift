ef297a94e787bd620fec7ca21bfd23844aa9f16d
Alexey Alter-Pesotskiy <alex@testableapple.com>
2024-12-06 10:48:51 +0000

    [CI] Update Xcode version (#620)

--- hooks/pre-commit.sh
+++ hooks/pre-commit.sh
@@ -3,12 +3,14 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
-# Define an array of paths
-paths=("Sources" "DemoApp" "DemoAppUIKit" "StreamVideoTests" "StreamVideoUIKitTests")
+if [ "${GITHUB_ACTIONS}" != "true" ]; then
+    # Define an array of paths
+    paths=("Sources" "DemoApp" "DemoAppUIKit" "StreamVideoTests" "StreamVideoUIKitTests")
 
-# Loop through each path
-for path in "${paths[@]}"; do
-    mint run swiftformat --lint --config .swiftformat \
-        --exclude "**/Generated","**/generated","**/protobuf","**/OpenApi" \
-        "$path"
-done
+    # Loop through each path
+    for path in "${paths[@]}"; do
+        mint run swiftformat --lint --config .swiftformat \
+            --exclude "**/Generated","**/generated","**/protobuf","**/OpenApi" \
+            "$path"
+    done
+fi
