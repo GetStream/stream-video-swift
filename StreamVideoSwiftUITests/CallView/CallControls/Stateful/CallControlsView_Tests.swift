aad0d38bc1c0bf60bde9a8b6cd56d517a251aae1
Ilias Pavlidakis <ilias.pavlidakis@getstream.io>
2025-02-07 17:25:45 +0200

    Update snapshots

--- StreamVideoSwiftUITests/CallView/CallControls/Stateful/CallControlsView_Tests.swift
+++ StreamVideoSwiftUITests/CallView/CallControls/Stateful/CallControlsView_Tests.swift
@@ -11,8 +11,41 @@ import XCTest
 @MainActor
 final class CallControlsView_Tests: StreamVideoUITestCase {
     
-    func test_callControlsView_snapshot() throws {
+    func test_callControlsView_withoutCapabilities_snapshot() throws {
         let view = CallControlsView(viewModel: CallViewModel())
-        AssertSnapshot(view, variants: snapshotVariants, size: sizeThatFits)
+        AssertSnapshot(
+            view,
+            variants: snapshotVariants,
+            size: sizeThatFits
+        )
+    }
+
+    func test_callControlsView_activeCall_snapshot() throws {
+        let call = streamVideoUI?.streamVideo.call(callType: .default, callId: .unique)
+        call?.state.ownCapabilities = [.sendAudio, .sendVideo]
+        let viewModel = CallViewModel()
+        viewModel.setActiveCall(call)
+
+        let view = CallControlsView(viewModel: viewModel)
+        AssertSnapshot(
+            view,
+            variants: snapshotVariants,
+            size: sizeThatFits
+        )
+    }
+
+    func test_callControlsView_ringingCall_snapshot() async throws {
+        let call = try XCTUnwrap(streamVideoUI?.streamVideo.call(callType: .default, callId: .unique))
+        call.state.ownCapabilities = [.sendAudio, .sendVideo]
+        streamVideoUI?.streamVideo.state.ringingCall = call
+        let viewModel = CallViewModel()
+        viewModel.callingState = .outgoing
+
+        let view = CallControlsView(viewModel: viewModel)
+        AssertSnapshot(
+            view,
+            variants: snapshotVariants,
+            size: sizeThatFits
+        )
     }
 }
