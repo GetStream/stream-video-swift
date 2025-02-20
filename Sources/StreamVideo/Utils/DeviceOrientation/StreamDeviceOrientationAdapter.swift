a742744c52fae0263697f5b144759bd846024072
Ilias Pavlidakis <ilias.pavlidakis@getstream.io>
2025-02-20 13:01:22 +0200

    Fix orientation issue

--- Sources/StreamVideo/Utils/DeviceOrientation/StreamDeviceOrientationAdapter.swift
+++ Sources/StreamVideo/Utils/DeviceOrientation/StreamDeviceOrientationAdapter.swift
@@ -7,6 +7,7 @@ import Foundation
 #if canImport(UIKit)
 import UIKit
 #endif
+import AVFoundation
 
 /// An enumeration representing device orientations: portrait or landscape.
 public enum StreamDeviceOrientation: Equatable, Sendable {
@@ -49,12 +50,21 @@ public enum StreamDeviceOrientation: Equatable, Sendable {
     public var deviceOrientation: UIDeviceOrientation {
         switch self {
         case let .portrait(isUpsideDown):
-            return isUpsideDown ? UIDeviceOrientation.portraitUpsideDown : .portrait
+            return isUpsideDown ? .portraitUpsideDown : .portrait
         case let .landscape(isLeft):
-            return isLeft ? UIDeviceOrientation.landscapeLeft : .landscapeRight
+            return isLeft ? .landscapeLeft : .landscapeRight
         }
     }
     #endif
+
+    public var captureVideoOrientation: AVCaptureVideoOrientation {
+        switch self {
+        case let .portrait(isUpsideDown):
+            return isUpsideDown ? .portraitUpsideDown : .portrait
+        case let .landscape(isLeft):
+            return isLeft ? .landscapeLeft : .landscapeRight
+        }
+    }
 }
 
 /// An observable object that adapts to device orientation changes.
