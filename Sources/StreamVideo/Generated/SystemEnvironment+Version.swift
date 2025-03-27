f66523641384ea529f75f5d66095277d18e4af28
Ilias Pavlidakis <ilias.pavlidakis@getstream.io>
2025-03-27 18:08:36 +0200

    Add snapshot postfix to v1.19.2

--- Sources/StreamVideo/Generated/SystemEnvironment+Version.swift
+++ Sources/StreamVideo/Generated/SystemEnvironment+Version.swift
@@ -7,7 +7,7 @@ import Foundation
 
 extension SystemEnvironment {
   /// A Stream Video version.
-  public static let version: String = "1.19.2"
+  public static let version: String = "1.19.2-SNAPSHOT"
   /// The WebRTC version.
   public static let webRTCVersion: String = "114.5735.8"
 }
