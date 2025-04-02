ed44fee0e4b921f485b305bf417ae0096836233e
Ilias Pavlidakis <ilias.pavlidakis@getstream.io>
2025-04-02 15:30:31 +0300

    Add tests

---
+++ StreamVideoTests/WebRTC/RTCConfiguration_DefaultsTests.swift
@@ -0,0 +1,23 @@
+//
+// Copyright © 2025 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+@testable import StreamVideo
+import StreamWebRTC
+import XCTest
+
+final class RTCConfiguration_DefaultsTests: XCTestCase, @unchecked Sendable {
+
+    func test_makeConfiguration_returnsCorrectlyConfiguredResult() {
+        let iceServer = ICEServer(password: .unique, urls: [], username: .unique)
+
+        let configuration = RTCConfiguration.makeConfiguration(with: [iceServer])
+
+        XCTAssertEqual(configuration.iceServers.count, 1)
+        XCTAssertEqual(configuration.iceServers[0].username, iceServer.username)
+        XCTAssertEqual(configuration.iceServers[0].credential, iceServer.password)
+        XCTAssertEqual(configuration.sdpSemantics, .unifiedPlan)
+        XCTAssertEqual(configuration.bundlePolicy, .maxBundle)
+    }
+}
