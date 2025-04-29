17bc7ca01fd09de335ed100d2455c45918ab5e89
Martin Mitrevski <martinmitrevski.oh@gmail.com>
2025-04-29 21:41:57 +0200

    Updated StatsOptions model (#789)

--- Sources/StreamVideo/OpenApi/generated/Models/StatsOptions.swift
+++ Sources/StreamVideo/OpenApi/generated/Models/StatsOptions.swift
@@ -6,21 +6,26 @@ import Foundation
 
 public final class StatsOptions: @unchecked Sendable, Codable, JSONEncodable, Hashable {
     
+    public var enableRtcStats: Bool
     public var reportingIntervalMs: Int
 
-    public init(reportingIntervalMs: Int) {
+    public init(enableRtcStats: Bool, reportingIntervalMs: Int) {
+        self.enableRtcStats = enableRtcStats
         self.reportingIntervalMs = reportingIntervalMs
     }
     
     public enum CodingKeys: String, CodingKey, CaseIterable {
+        case enableRtcStats = "enable_rtc_stats"
         case reportingIntervalMs = "reporting_interval_ms"
     }
     
     public static func == (lhs: StatsOptions, rhs: StatsOptions) -> Bool {
-        lhs.reportingIntervalMs == rhs.reportingIntervalMs
+        lhs.enableRtcStats == rhs.enableRtcStats &&
+            lhs.reportingIntervalMs == rhs.reportingIntervalMs
     }
 
     public func hash(into hasher: inout Hasher) {
+        hasher.combine(enableRtcStats)
         hasher.combine(reportingIntervalMs)
     }
 }
