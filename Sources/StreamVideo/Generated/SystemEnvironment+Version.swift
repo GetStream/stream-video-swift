ede0c802a8aec5956f48746956d26f451e55853b
Ilias Pavlidakis <ilias.pavlidakis@getstream.io>
2025-05-27 19:47:39 +0300

    Update SystemEnvironment

--- Sources/StreamVideo/Generated/SystemEnvironment+Version.swift
+++ Sources/StreamVideo/Generated/SystemEnvironment+Version.swift
@@ -9,5 +9,5 @@ extension SystemEnvironment {
   /// A Stream Video version.
   public static let version: String = "1.24.0-SNAPSHOT"
   /// The WebRTC version.
-  public static let webRTCVersion: String = "114.5735.8"
+  public static let webRTCVersion: String = "125.6422.70"
 }
