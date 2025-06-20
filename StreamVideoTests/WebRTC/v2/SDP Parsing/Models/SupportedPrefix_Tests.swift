6064c3651ed6932de4454754f597af52c1072182
Ilias Pavlidakis <ilias.pavlidakis@getstream.io>
2025-06-20 18:44:15 +0300

    [Feature]Enable Stereo for subscriber connections if possible

--- StreamVideoTests/WebRTC/v2/SDP Parsing/Models/SupportedPrefix_Tests.swift
+++ StreamVideoTests/WebRTC/v2/SDP Parsing/Models/SupportedPrefix_Tests.swift
@@ -11,12 +11,18 @@ final class SupportedPrefix_Tests: XCTestCase, @unchecked Sendable {
 
     func test_supportedPrefixCases() {
         XCTAssertEqual(SupportedPrefix.rtmap.rawValue, "a=rtpmap:")
+        XCTAssertEqual(SupportedPrefix.media.rawValue, "m=")
+        XCTAssertEqual(SupportedPrefix.mid.rawValue, "a=mid:")
+        XCTAssertEqual(SupportedPrefix.fmtp.rawValue, "a=fmtp:")
     }
 
     // MARK: - SupportedPrefix Set
 
     func test_supportedPrefixSetContainsRtmap() {
-        let supportedPrefixes: Set<SupportedPrefix> = [.rtmap]
+        let supportedPrefixes: Set<SupportedPrefix> = [.rtmap, .media, .mid, .fmtp]
         XCTAssertTrue(supportedPrefixes.contains(.rtmap))
+        XCTAssertTrue(supportedPrefixes.contains(.media))
+        XCTAssertTrue(supportedPrefixes.contains(.mid))
+        XCTAssertTrue(supportedPrefixes.contains(.fmtp))
     }
 }
