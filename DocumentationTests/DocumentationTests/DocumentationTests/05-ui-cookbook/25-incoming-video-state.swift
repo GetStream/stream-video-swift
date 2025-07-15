5dfdc124195a5999fa28bb0c3e87460bfce0338f
Ilias Pavlidakis <ilias.pavlidakis@getstream.io>
2025-07-15 15:02:43 +0300

    Update docs

--- DocumentationTests/DocumentationTests/DocumentationTests/05-ui-cookbook/25-incoming-video-state.swift
+++ DocumentationTests/DocumentationTests/DocumentationTests/05-ui-cookbook/25-incoming-video-state.swift
@@ -7,7 +7,7 @@ import Combine
 fileprivate func content() {
     asyncContainer {
         let call = streamVideo.call(callType: "default", callId: "my-call-id")
-        await call.updateClientCapabilities([.subscriberVideoPause])
+        await call.updateClientCapabilities([])
     }
 
     container {
@@ -24,5 +24,145 @@ fileprivate func content() {
 
         // Cancel when no longer needed:
         cancellable.cancel()
+    }
+
+    viewContainer {
+        if participant.pausedTracks.contains(.video) {
+            Image(systemName: "video.slash.fill")
+                .foregroundColor(.yellow)
+                .padding(4)
+        }
+    }
+
+    container {
+        struct ParticipantInfoView: View {
+            @Injected(\.images) var images
+            @Injected(\.fonts) var fonts
+            @Injected(\.colors) var colors
+
+            var participant: CallParticipant
+            var isPinned: Bool
+            var maxHeight: CGFloat
+
+            public init(
+                participant: CallParticipant,
+                isPinned: Bool,
+                maxHeight: Float = 14
+            ) {
+                self.participant = participant
+                self.isPinned = isPinned
+                self.maxHeight = CGFloat(maxHeight)
+            }
+
+            public var body: some View {
+                HStack(spacing: 4) {
+                    if isPinned {
+                        Image(systemName: "pin.fill")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(maxHeight: maxHeight)
+                            .foregroundColor(.white)
+                            .padding(.trailing, 4)
+                    }
+                    Text(participant.name.isEmpty ? participant.id : participant.name)
+                        .foregroundColor(.white)
+                        .multilineTextAlignment(.leading)
+                        .lineLimit(1)
+                        .font(fonts.caption1)
+                        .minimumScaleFactor(0.7)
+                        .accessibility(identifier: "participantName")
+
+                    if participant.pausedTracks.contains(.video) {
+                        Image(systemName: "wifi.slash")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(maxHeight: maxHeight)
+                            .foregroundColor(.white)
+                            .padding(.trailing, 4)
+                    }
+
+                    SoundIndicator(participant: participant)
+                        .frame(maxHeight: maxHeight)
+                }
+                .padding(.all, 2)
+                .padding(.horizontal, 4)
+                .frame(height: 28)
+                .cornerRadius(
+                    8,
+                    corners: [.topRight],
+                    backgroundColor: colors.participantInfoBackgroundColor
+                )
+            }
+        }
+    }
+
+    container {
+        struct VideoCallParticipantModifier: ViewModifier {
+
+            var participant: CallParticipant
+            var call: Call?
+            var availableFrame: CGRect
+            var ratio: CGFloat
+            var showAllInfo: Bool
+            var decorations: Set<VideoCallParticipantDecoration>
+
+            public init(
+                participant: CallParticipant,
+                call: Call?,
+                availableFrame: CGRect,
+                ratio: CGFloat,
+                showAllInfo: Bool,
+                decorations: [VideoCallParticipantDecoration] = VideoCallParticipantDecoration.allCases
+            ) {
+                self.participant = participant
+                self.call = call
+                self.availableFrame = availableFrame
+                self.ratio = ratio
+                self.showAllInfo = showAllInfo
+                self.decorations = .init(decorations)
+            }
+
+            public func body(content: Content) -> some View {
+                content
+                    .adjustVideoFrame(to: availableFrame.size.width, ratio: ratio)
+                    .overlay(
+                        ZStack {
+                            BottomView(content: {
+                                HStack {
+                                    ParticipantInfoView(
+                                        participant: participant,
+                                        isPinned: participant.isPinned
+                                    )
+
+                                    Spacer()
+
+                                    if showAllInfo {
+                                        ConnectionQualityIndicator(
+                                            connectionQuality: participant.connectionQuality
+                                        )
+                                    }
+                                }
+                            })
+                        }
+                    )
+                    .applyDecorationModifierIfRequired(
+                        VideoCallParticipantOptionsModifier(participant: participant, call: call),
+                        decoration: .options,
+                        availableDecorations: decorations
+                    )
+                    .applyDecorationModifierIfRequired(
+                        VideoCallParticipantSpeakingModifier(participant: participant, participantCount: participantCount),
+                        decoration: .speaking,
+                        availableDecorations: decorations
+                    )
+                    .clipShape(RoundedRectangle(cornerRadius: 16))
+                    .clipped()
+            }
+
+            @MainActor
+            private var participantCount: Int {
+                call?.state.participants.count ?? 0
+            }
+        }
     }
 }
