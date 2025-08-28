261d0aa433f19d9fd9430a2d24f5b6317f917d36
Ilias Pavlidakis <ilias.pavlidakis@getstream.io>
2025-08-28 12:32:49 +0300

    Fix flakiness in ests

--- StreamVideoTests/Utils/PermissionStore/PermissionStore_Tests.swift
+++ StreamVideoTests/Utils/PermissionStore/PermissionStore_Tests.swift
@@ -10,10 +10,11 @@ final class PermissionStore_Tests: XCTestCase, @unchecked Sendable {
 
     private lazy var mockAudioStore: MockRTCAudioStore! = .init()
     private lazy var mockReducer: MockReducer<PermissionStore.Namespace>! = .init()
+    private lazy var mockMiddleware: MockPermissionStoreMiddleware = .init()
     private lazy var store: Store<PermissionStore.Namespace>! = PermissionStore.Namespace.store(
         initialState: .initial,
         reducers: PermissionStore.Namespace.reducers() + [mockReducer],
-        middleware: [] // We are disabling middleware to avoid system triggers
+        middleware: [mockMiddleware] // We are disabling middleware to avoid system triggers
     )
     private lazy var subject: PermissionStore! = .init(store: store)
 
@@ -85,61 +86,82 @@ final class PermissionStore_Tests: XCTestCase, @unchecked Sendable {
 
     func test_requestMicrophonePermission_dispatchesExpectedAction() async throws {
         await wait(for: 1.0)
-        store.dispatch(.setMicrophonePermission(.granted))
 
         _ = try await subject.requestMicrophonePermission()
-        await fulfillment(timeout: 2) { self.mockReducer.inputs.count > 1 }
-        XCTAssertTrue(mockReducer.inputs.count > 1)
-
-        let requestMicrophonePermission = mockReducer
-            .inputs
-            .filter {
-                guard case .requestMicrophonePermission = $0.action else {
-                    return false
+        await fulfillment {
+            let requestMicrophonePermission = self.mockReducer
+                .inputs
+                .filter {
+                    guard case .requestMicrophonePermission = $0.action else {
+                        return false
+                    }
+                    return true
                 }
-                return true
-            }
-        XCTAssertEqual(requestMicrophonePermission.count, 1)
+            return requestMicrophonePermission.count == 1
+        }
     }
 
     // MARK: - requestCameraPermission
 
     func test_requestCameraPermission_dispatchesExpectedAction() async throws {
         await wait(for: 1.0)
-        store.dispatch(.setCameraPermission(.granted))
 
         _ = try await subject.requestCameraPermission()
-        await fulfillment(timeout: 2) { self.mockReducer.inputs.count > 1 }
-        XCTAssertTrue(mockReducer.inputs.count > 1)
-
-        let requestCameraPermission = mockReducer
-            .inputs
-            .filter {
-                guard case .requestCameraPermission = $0.action else {
-                    return false
+        await fulfillment(timeout: 2) {
+            let requestCameraPermission = self.mockReducer
+                .inputs
+                .filter {
+                    guard case .requestCameraPermission = $0.action else {
+                        return false
+                    }
+                    return true
                 }
-                return true
-            }
-        XCTAssertEqual(requestCameraPermission.count, 1)
+            return requestCameraPermission.count == 1
+        }
     }
 
     // MARK: - requestPushNotificationPermission
 
     func test_requestPushNotificationPermission_dispatchesExpectedAction() async throws {
         await wait(for: 1.0)
-        store.dispatch(.setPushNotificationPermission(.granted))
 
         _ = try await subject.requestPushNotificationPermission(with: [])
-        await fulfillment { self.mockReducer.inputs.count > 1 }
-
-        let requestPushNotificationPermission = mockReducer
-            .inputs
-            .filter {
-                guard case .requestPushNotificationPermission = $0.action else {
-                    return false
+        await fulfillment {
+            let requestPushNotificationPermission = self.mockReducer
+                .inputs
+                .filter {
+                    guard case .requestPushNotificationPermission = $0.action else {
+                        return false
+                    }
+                    return true
                 }
-                return true
-            }
-        XCTAssertEqual(requestPushNotificationPermission.count, 1)
+            return requestPushNotificationPermission.count == 1
+        }
+    }
+}
+
+final class MockPermissionStoreMiddleware: Middleware<PermissionStore.Namespace>, @unchecked Sendable {
+
+    var stubMicrophonePermission: PermissionStore.Permission = .granted
+    var stubCameraPermission: PermissionStore.Permission = .granted
+    var stubPushNotificationsPermission: PermissionStore.Permission = .granted
+
+    override func apply(
+        state: PermissionStore.StoreState,
+        action: PermissionStore.StoreAction,
+        file: StaticString,
+        function: StaticString,
+        line: UInt
+    ) {
+        switch action {
+        case .requestMicrophonePermission:
+            dispatcher?.dispatch(.setMicrophonePermission(stubMicrophonePermission))
+        case .requestCameraPermission:
+            dispatcher?.dispatch(.setCameraPermission(stubCameraPermission))
+        case .requestPushNotificationPermission:
+            dispatcher?.dispatch(.setPushNotificationPermission(stubPushNotificationsPermission))
+        default:
+            break
+        }
     }
 }
