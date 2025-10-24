9f8107dc8b1fd6bcc4800e5247db35619a71431e
Alexey Alter-Pesotskiy <alex@testableapple.com>
2025-10-24 17:23:47 +0100

    Update release version to snapshot

--- Sources/StreamVideo/Generated/SystemEnvironment+Version.swift
+++ Sources/StreamVideo/Generated/SystemEnvironment+Version.swift
@@ -7,7 +7,7 @@ import Foundation
 
 extension SystemEnvironment {
   /// A Stream Video version.
-  public static let version: String = "1.34.2"
+  public static let version: String = "1.35.0-SNAPSHOT"
   /// The WebRTC version.
   public static let webRTCVersion: String = "137.0.42"
 }
