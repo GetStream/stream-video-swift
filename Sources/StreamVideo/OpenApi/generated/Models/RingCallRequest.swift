98dc9d704557367493e7511b63903ad49f60b11c
Martin Mitrevski <martinmitrevski.oh@gmail.com>
2025-11-10 12:00:44 +0100

    Added endpoint for ringing members (#992)

---
+++ Sources/StreamVideo/OpenApi/generated/Models/RingCallRequest.swift
@@ -0,0 +1,30 @@
+//
+// Copyright © 2025 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+
+public final class RingCallRequest: @unchecked Sendable, Codable, JSONEncodable, Hashable {
+    public var membersIds: [String]?
+    public var video: Bool?
+
+    public init(membersIds: [String]? = nil, video: Bool? = nil) {
+        self.membersIds = membersIds
+        self.video = video
+    }
+
+public enum CodingKeys: String, CodingKey, CaseIterable {
+    case membersIds = "members_ids"
+    case video
+}
+
+    public static func == (lhs: RingCallRequest, rhs: RingCallRequest) -> Bool {
+        lhs.membersIds == rhs.membersIds &&
+        lhs.video == rhs.video
+    }
+
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(membersIds)
+        hasher.combine(video)
+    }
+}
