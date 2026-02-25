f4422575e42275df9ee4ea51c2cb79cc74aeb147
Martin Mitrevski <martinmitrevski.oh@gmail.com>
2026-02-25 17:19:51 +0100

    Add migrating from list in join call request

--- Sources/StreamVideo/OpenApi/generated/Models/JoinCallRequest.swift
+++ Sources/StreamVideo/OpenApi/generated/Models/JoinCallRequest.swift
@@ -5,53 +5,47 @@
 import Foundation
 
 public final class JoinCallRequest: @unchecked Sendable, Codable, JSONEncodable, Hashable {
-    
     public var create: Bool?
     public var data: CallRequest?
     public var location: String
     public var membersLimit: Int?
     public var migratingFrom: String?
+    public var migratingFromList: [String]?
     public var notify: Bool?
     public var ring: Bool?
     public var video: Bool?
 
-    public init(
-        create: Bool? = nil,
-        data: CallRequest? = nil,
-        location: String,
-        membersLimit: Int? = nil,
-        migratingFrom: String? = nil,
-        notify: Bool? = nil,
-        ring: Bool? = nil,
-        video: Bool? = nil
-    ) {
+    public init(create: Bool? = nil, data: CallRequest? = nil, location: String, membersLimit: Int? = nil, migratingFrom: String? = nil, migratingFromList: [String]? = nil, notify: Bool? = nil, ring: Bool? = nil, video: Bool? = nil) {
         self.create = create
         self.data = data
         self.location = location
         self.membersLimit = membersLimit
         self.migratingFrom = migratingFrom
+        self.migratingFromList = migratingFromList
         self.notify = notify
         self.ring = ring
         self.video = video
     }
-    
+
     public enum CodingKeys: String, CodingKey, CaseIterable {
         case create
         case data
         case location
         case membersLimit = "members_limit"
         case migratingFrom = "migrating_from"
+        case migratingFromList = "migrating_from_list"
         case notify
         case ring
         case video
     }
-    
+
     public static func == (lhs: JoinCallRequest, rhs: JoinCallRequest) -> Bool {
         lhs.create == rhs.create &&
             lhs.data == rhs.data &&
             lhs.location == rhs.location &&
             lhs.membersLimit == rhs.membersLimit &&
             lhs.migratingFrom == rhs.migratingFrom &&
+            lhs.migratingFromList == rhs.migratingFromList &&
             lhs.notify == rhs.notify &&
             lhs.ring == rhs.ring &&
             lhs.video == rhs.video
@@ -63,6 +57,7 @@ public final class JoinCallRequest: @unchecked Sendable, Codable, JSONEncodable,
         hasher.combine(location)
         hasher.combine(membersLimit)
         hasher.combine(migratingFrom)
+        hasher.combine(migratingFromList)
         hasher.combine(notify)
         hasher.combine(ring)
         hasher.combine(video)
