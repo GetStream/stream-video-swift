1f49f27f798d51d56671f1e7fc9c744b86843e3b
Ilias Pavlidakis <ilias.pavlidakis@getstream.io>
2026-02-26 16:47:06 +0200

    Update WebRTC to 137.0.67

--- Sources/StreamVideo/Generated/SystemEnvironment+Version.swift
+++ Sources/StreamVideo/Generated/SystemEnvironment+Version.swift
@@ -9,5 +9,5 @@ extension SystemEnvironment {
   /// A Stream Video version.
   public static let version: String = "1.43.0-SNAPSHOT"
   /// The WebRTC version.
-  public static let webRTCVersion: String = "137.0.66"
+  public static let webRTCVersion: String = "137.0.67"
 }
